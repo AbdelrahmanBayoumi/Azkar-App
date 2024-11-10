30c149e8a0692ad8a2f33bca626ef80621d484a5
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2024-11-10 16:23:11 +0200

    Update Constants.java

--- Azkar/src/main/java/com/bayoumi/util/Constants.java
+++ Azkar/src/main/java/com/bayoumi/util/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public final static String VERSION = "1.2.9";
     public final static String LOCATIONS_DB_URL = "https://github.com/AbdelrahmanBayoumi/LocationsDB/releases/latest/download/locations.db";
     public static final String QURAN_FONT_FAMILY = "Noto Naskh Arabic";
-    public final static Mode RUNNING_MODE = Mode.PRODUCTION;
+    public final static Mode RUNNING_MODE = Mode.DEVELOPMENT;
 
     static {
         try {
