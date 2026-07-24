ac7ff5a34e0dd6bcac7d6b1bd29130414c0803f4
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2026-07-24 22:33:51 +0300

    remove runAtStartup feature for Linux

--- Azkar/src/main/java/com/bayoumi/controllers/settings/other/OtherSettingsController.java
+++ Azkar/src/main/java/com/bayoumi/controllers/settings/other/OtherSettingsController.java
@@ -9,7 +9,6 @@ import com.bayoumi.models.settings.Settings;
 import com.bayoumi.services.update.UpdateHandler;
 import com.bayoumi.util.Constants;
 import com.bayoumi.util.Logger;
-import com.bayoumi.util.StartupUtil;
 import com.bayoumi.util.Utility;
 import com.bayoumi.util.gui.BuilderUI;
 import com.bayoumi.util.gui.HelperMethods;
@@ -47,7 +46,7 @@ public class OtherSettingsController implements Initializable, SettingsInterface
     @FXML
     private Spinner<Integer> hijriDateOffset;
     @FXML
-    private Label minimizeAtStart, runAtStartup, format24, darkTheme, hijriDateLabel, version, adjustingTheHijriDateText, languageText, adjustingTheHijriDateNote, usageStatsLabel,
+    private Label minimizeAtStart, format24, darkTheme, hijriDateLabel, version, adjustingTheHijriDateText, languageText, adjustingTheHijriDateNote, usageStatsLabel,
             versionNumberLabel, website, termsOfUse, privacyPolicy, shareLabel;
     @FXML
     private VBox scrollChild, loadingBox;
@@ -56,7 +55,7 @@ public class OtherSettingsController implements Initializable, SettingsInterface
     @FXML
     private ScrollPane scrollPane;
     @FXML
-    private JFXToggleButton minimizeAtStartToggle, runAtStartupToggle, format24Toggle, darkThemeToggle;
+    private JFXToggleButton minimizeAtStartToggle, format24Toggle, darkThemeToggle;
 
 
     public void updateBundle(ResourceBundle bundle) {
@@ -65,7 +64,6 @@ public class OtherSettingsController implements Initializable, SettingsInterface
         format24.setText(Utility.toUTF(bundle.getString("hour24System")));
         darkTheme.setText(Utility.toUTF(bundle.getString("darkTheme")));
         minimizeAtStart.setText(Utility.toUTF(bundle.getString("minimizeAtStart")));
-        runAtStartup.setText(Utility.toUTF(bundle.getString("runAtStartup")));
         adjustingTheHijriDateText.setText(Utility.toUTF(bundle.getString("adjustingTheHijriDateText")));
         adjustingTheHijriDateNote.setText(Utility.toUTF(bundle.getString("adjustingTheHijriDateNote")));
         checkForUpdateButton.setText(Utility.toUTF(bundle.getString("checkForUpdate")));
@@ -85,7 +83,6 @@ public class OtherSettingsController implements Initializable, SettingsInterface
 
         toggleAction(format24Toggle);
         toggleAction(minimizeAtStartToggle);
-        toggleAction(runAtStartupToggle);
         toggleAction(darkThemeToggle);
     }
 
@@ -109,12 +106,6 @@ public class OtherSettingsController implements Initializable, SettingsInterface
 
             format24Toggle.setSelected(settings.getEnable24Format());
             minimizeAtStartToggle.setSelected(settings.getMinimized());
-
-            boolean startupEnabled = StartupUtil.isAutostartEnabled();
-            runAtStartupToggle.setSelected(startupEnabled);
-            if (startupEnabled != settings.getRunAtStartup()) {
-                settings.setRunAtStartup(startupEnabled);
-            }
 
             darkThemeToggle.setSelected(settings.getNightMode());
 
@@ -264,13 +255,6 @@ public class OtherSettingsController implements Initializable, SettingsInterface
     private void minimizeAtStartSelect() {
         toggleAction(minimizeAtStartToggle);
         Settings.getInstance().setMinimized(minimizeAtStartToggle.isSelected());
-    }
-
-    @FXML
-    private void runAtStartupSelect() {
-        toggleAction(runAtStartupToggle);
-        Settings.getInstance().setRunAtStartup(runAtStartupToggle.isSelected());
-        StartupUtil.setAutostart(runAtStartupToggle.isSelected());
     }
 
     private void toggleAction(JFXToggleButton toggleButton) {
--- Azkar/src/main/java/com/bayoumi/models/settings/Settings.java
+++ Azkar/src/main/java/com/bayoumi/models/settings/Settings.java
@@ -30,7 +30,6 @@ public class Settings extends PreferencesObservable {
     private final SimpleEntry<PreferencesType, Language> language;
     private final SimpleEntry<PreferencesType, Integer> hijriOffset;
     private final SimpleEntry<PreferencesType, Boolean> sendUsageData;
-    private final SimpleEntry<PreferencesType, Boolean> runAtStartup;
 
 
     private Settings() {
@@ -44,7 +43,6 @@ public class Settings extends PreferencesObservable {
         language = new SimpleEntry<>(PreferencesType.LANGUAGE, Language.get(PreferencesType.LANGUAGE.getDefaultValue()));
         hijriOffset = new SimpleEntry<>(PreferencesType.HIJRI_OFFSET, Integer.valueOf(PreferencesType.HIJRI_OFFSET.getDefaultValue()));
         sendUsageData = new SimpleEntry<>(PreferencesType.SEND_USAGE_DATA, Boolean.valueOf(PreferencesType.SEND_USAGE_DATA.getDefaultValue()));
-        runAtStartup = new SimpleEntry<>(PreferencesType.RUN_AT_STARTUP, Boolean.valueOf(PreferencesType.RUN_AT_STARTUP.getDefaultValue()));
 
         loadSettings();
     }
@@ -57,7 +55,6 @@ public class Settings extends PreferencesObservable {
         language.setValue(Language.get(Preferences.getInstance().get(language.getKey())));
         hijriOffset.setValue(Preferences.getInstance().getInt(hijriOffset.getKey()));
         sendUsageData.setValue(Preferences.getInstance().getBoolean(sendUsageData.getKey()));
-        runAtStartup.setValue(Preferences.getInstance().getBoolean(runAtStartup.getKey()));
     }
 
     public boolean getSendUsageData() {
@@ -157,19 +154,6 @@ public class Settings extends PreferencesObservable {
         Preferences.getInstance().set(automaticCheckForUpdates.getKey(), value + "");
         // 3. notify observers
         notifyObservers(automaticCheckForUpdates.getKey(), value);
-    }
-
-    public boolean getRunAtStartup() {
-        return runAtStartup.getValue();
-    }
-
-    public void setRunAtStartup(boolean value) {
-        // 1. set value to local variable
-        runAtStartup.setValue(value);
-        // 2. save value to DB
-        Preferences.getInstance().set(runAtStartup.getKey(), value + "");
-        // 3. notify observers
-        notifyObservers(runAtStartup.getKey(), value);
     }
 
     public AzkarSettings getAzkarSettings() {
